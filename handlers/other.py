d7c548eadf04445cd03b33662cc07aa683c5d2aa
Ilya-afk <ivodolazky@mail.ru>
2022-03-10 15:24:02 +0300

    lambda and filter example

--- handlers/other.py
+++ handlers/other.py
@@ -2,12 +2,25 @@ import json
 import string
 
 from aiogram import types, Dispatcher
+from aiogram.dispatcher.filters import Text
 
 from create_bot import dp
 
 
+# так можно избавиться от / и сделать более красиво
+# @dp.message_handler(lambda message: 'пиво' in message.text)
+async def beer(message: types.Message):
+    await message.answer('ты в пиве')
+
+
+# или так
+# @dp.message_handler(Text(equals='аниме', ignore_case=True))
+async def anime(message: types.Message):
+    await message.answer('Твое имя')
+
+
 #@dp.message_handler()
-async def echo_send(message: types.Message):
+async def message_filter(message: types.Message):
     if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')} \
             .intersection(set(json.load(open('black_list.json')))) != set():
         await message.reply('запрещенное слово!')
@@ -15,4 +28,6 @@ async def echo_send(message: types.Message):
 
 
 def register_handlers_other(dp: Dispatcher):
-    dp.register_message_handler(echo_send)
+    dp.register_message_handler(beer, lambda message: 'пиво' in message.text)
+    dp.register_message_handler(anime, Text(equals='аниме', ignore_case=True))
+    dp.register_message_handler(message_filter)
